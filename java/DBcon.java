75e0dface186c853cac764ccc94ed0b76c2d7ddb
SaiKrishna <82310190+SaikrishnaBijja@users.noreply.github.com>
2024-03-09 09:20:15 +0530

    Add files via upload

---
+++ java/DBcon.java
@@ -0,0 +1,22 @@
+package p1;
+import java.sql.*;
+public class DBcon {
+		
+	public static Connection con;
+	public static Statement st;
+	public static int currentid;
+	public static void connect()
+	{
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			 con=DriverManager.getConnection("jdbc:mysql://localhost:3306/blog","root","tiger");
+			st=con.createStatement();
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
+
+}
